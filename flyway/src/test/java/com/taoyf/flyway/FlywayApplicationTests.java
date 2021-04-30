f3b22be8b07dde2d450538c065defbb3d5a58fb7
tyfqwer <1442282810@qq.com>
2021-04-30 13:49:25 +0800

    整合flyway

---
+++ flyway/src/test/java/com/taoyf/flyway/FlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoyf.flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
